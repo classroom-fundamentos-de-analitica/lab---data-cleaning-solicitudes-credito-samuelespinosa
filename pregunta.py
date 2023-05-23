096cd0bb78c63759992f55093ae6646c0b4bace7
samuel <saespinosab@unal.edu.co>
2023-05-22 22:07:46 -0500

    1

--- pregunta.py
+++ pregunta.py
@@ -7,14 +7,20 @@ correctamente. Tenga en cuenta datos faltantes y duplicados.
 
 """
 import pandas as pd
-
+from re import match 
+from datetime import datetime
 
 def clean_data():
-
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
-
-    #
-    # Inserte su código aquí
-    #
-
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col=0)
+    df=df.dropna()
+    df['sexo'] = df.sexo.str.lower()
+    df['tipo_de_emprendimiento'] = df.tipo_de_emprendimiento.str.lower()
+    df['idea_negocio']=df['idea_negocio'].apply(lambda x: x.lower().replace("_", " ").replace("-", " "))
+    df.barrio = [str.lower(barrio).replace("_", " ").replace("-", " ") for barrio in df.barrio]
+    df['barrio']=df['barrio'].apply(lambda x: x.lower().replace("_"," ").replace("-"," "))
+    df['línea_credito']=df['línea_credito'].apply(lambda x: x.lower().replace("-"," ").replace("_"," "))
+    df['comuna_ciudadano'] = df.comuna_ciudadano.astype(int)
+    df['monto_del_credito']=df['monto_del_credito'].apply(lambda x: x.strip("$").replace(",","")).astype(float)
+    df['fecha_de_beneficio'] =df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x,"%d/%m/%Y") if match(r"\d{1,2}/\d{2}/\d{4}", x) else datetime.strptime(x,"%Y/%m/%d"))
+    df=df.drop_duplicates()
     return df
